200a298c33e23b704c901bac60fabe36e1724661
ZengWeimao <so.zengtao@gmail.com>
2015-11-03 11:43:43 +0800

    清楚Navicat

---
+++ clearNavicat.sh
@@ -0,0 +1,5 @@
+rm -Rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat*
+rm -Rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/preference.plist
+rm -Rf ~/Library/Caches/com.prect.NavicatPremium 
+rm -Rf ~/Library/Preferences/com.prect.NavicatPremium.plist
+sudo rm -Rf /Applications/Navicat\ Premium.app/
